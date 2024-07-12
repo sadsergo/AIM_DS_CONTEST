2f5ffb6743b8d2841788639b23927214777eca8a
Sergei Nikitin <sergey.nikitin2013@gmail.com>
2024-07-12 15:41:38 +0300

    test: test multiprocessing

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from solution import number_of_patterns
 from tests import run_all_tests, run
 
+from mp_test import test
+
 import numpy as np
 
-
 def main():
-    run_all_tests()
+    # run_all_tests()
+    test()
     
 
 if __name__ == "__main__":
